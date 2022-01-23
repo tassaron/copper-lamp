ba32c410c6e0089278279670799673973572284f
tassaron <rutachigal@gmail.com>
2022-01-23 12:52:46 -0500

    powered copper block can deal more damage if left to charge

--- src/main/java/com/tassaron/copperlamp/block/PoweredCopperBlock.java
+++ src/main/java/com/tassaron/copperlamp/block/PoweredCopperBlock.java
@@ -6,40 +6,14 @@ import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.particle.ParticleTypes;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
-import java.util.Random;
 
 public class PoweredCopperBlock extends BlockWithEntity implements BlockEntityProvider {
 
     public PoweredCopperBlock(AbstractBlock.Settings settings) {
             super(settings);
     }
-
-    public PoweredCopperBlockEntity getBlockEntity(World world, BlockPos pos) {
-        return (PoweredCopperBlockEntity) world.getBlockEntity(pos);
-    }
-
-    public boolean isPowered(PoweredCopperBlockEntity be) {
-        if (be.energyStorage.getAmount() < 1) {
-            return false;
-        }
-        return true;
-    }
-
-    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (!isPowered(getBlockEntity(world, pos))) {
-            return;
-        }
-        BlockPos blockPos = pos.up();
-        world.spawnParticles(ParticleTypes.ELECTRIC_SPARK, (double)blockPos.getX() + 0.5D, (double)blockPos.getY() + 0.25D, (double)blockPos.getZ() + 0.5D, 8, 0.5D, 0.25D, 0.5D, 0.0D);
-    }
-
-
-    // BlockEntity-related Methods
 
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
@@ -48,7 +22,6 @@ public class PoweredCopperBlock extends BlockWithEntity implements BlockEntityPr
 
     @Override
     public BlockRenderType getRenderType(BlockState state) {
-        // With inheriting from BlockWithEntity this defaults to INVISIBLE, so we need to change that!
         return BlockRenderType.MODEL;
     }
 
--- src/main/java/com/tassaron/copperlamp/blockentity/PoweredCopperBlockEntity.java
+++ src/main/java/com/tassaron/copperlamp/blockentity/PoweredCopperBlockEntity.java
@@ -8,18 +8,18 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.Items;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-import team.reborn.energy.api.EnergyStorage;
-import team.reborn.energy.api.EnergyStorageUtil;
 import team.reborn.energy.api.base.SimpleEnergyStorage;
 
 import java.util.List;
 
 public class PoweredCopperBlockEntity extends BlockEntity {
-    public final SimpleEnergyStorage energyStorage = new SimpleEnergyStorage(1, 1, 1) {
+    private short tickDelay = 0;
+    public final SimpleEnergyStorage energyStorage = new SimpleEnergyStorage(10, 10, 10) {
         @Override
         protected void onFinalCommit() {
             markDirty();
@@ -35,42 +35,36 @@ public class PoweredCopperBlockEntity extends BlockEntity {
         int i = pos.getX();
         int j = pos.getY();
         int k = pos.getZ();
-        return (new Box((double)i, (double)j, (double)k, (double)(i + 1), (double)(j + 1), (double)(k + 1)));
+        return (new Box(i, j, k, i + 1, j + 1, k + 1));
     }
 
     public static void tick(World world, BlockPos pos, BlockState state, PoweredCopperBlockEntity be) {
         if (world == null || world.isClient) {
             return;
+        } else if (be.tickDelay > 0) {
+            be.tickDelay--;
+            return;
         }
-        CopperLampMod.LOGGER.info("Powered copper block has {} energy", be.energyStorage.getAmount());
         try (Transaction transaction = Transaction.openOuter()) {
-            // Use energy and extract more energy from adjacent blocks
             long amountUsed = 0;
-            long amountGained = 0;
             List<LivingEntity> nearbyEntities = world.getEntitiesByClass(LivingEntity.class, getAttackZone(pos), (entity) -> true);
             if (nearbyEntities.size() > 0) {
-                amountUsed = be.energyStorage.extract(1, transaction);
+                amountUsed = be.energyStorage.extract(10, transaction);
             }
-            for (Direction side : Direction.values()) {
-                amountGained = EnergyStorageUtil.move(
-                        EnergyStorage.SIDED.find(world, pos.offset(side), side.getOpposite()),
-                        be.energyStorage,
-                        1,
-                        transaction
+            if (amountUsed > 0) {
+                CopperLampMod.LOGGER.info(
+                        "Powered copper block at {},{},{} used {} energy",
+                        pos.getX(), pos.getY(), pos.getZ(), amountUsed
                 );
-                if (amountUsed > 0 || amountGained > 0) {
-                    CopperLampMod.LOGGER.info("Powered copper block used {} energy and gained {} energy", amountUsed, amountGained);
-                    transaction.commit();
-                    be.markDirty();
-                    break;
-                }
-            }
-            if (amountUsed > 0){
+                transaction.commit();
+                be.markDirty();
                 for (LivingEntity entity : nearbyEntities) {
                     if (!entity.getEquippedStack(EquipmentSlot.FEET).isOf(Items.LEATHER_BOOTS)) {
-                        entity.damage(DamageSource.LIGHTNING_BOLT, 1.0F);
+                        entity.damage(DamageSource.LIGHTNING_BOLT, (float) amountUsed);
+                        ((ServerWorld) world).spawnParticles(ParticleTypes.ELECTRIC_SPARK, entity.getX(), entity.getY(), entity.getZ(), 4, 0.5D, 0.25D, 0.5D, 0.0D);
                     }
                 }
+                be.tickDelay = 20;
             }
         }
     }
