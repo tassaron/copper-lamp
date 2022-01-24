b1d8ec3288e6d6ed4f2526a0f1f581f8658cceab
tassaron <rutachigal@gmail.com>
2022-01-23 23:46:50 -0500

    unlit and lit copper torch

--- src/main/java/com/tassaron/copperlamp/block/CopperTorchBlock.java
+++ src/main/java/com/tassaron/copperlamp/block/CopperTorchBlock.java
@@ -2,15 +2,10 @@ package com.tassaron.copperlamp.block;
 
 import com.tassaron.copperlamp.CopperLampMod;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.TorchBlock;
-import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
@@ -20,17 +15,18 @@ import team.reborn.energy.api.base.SimpleEnergyStorage;
 import java.util.Random;
 
 public class CopperTorchBlock extends TorchBlock {
-    public static final BooleanProperty LIT;
     public static final short tickDelay = 20;
+    public boolean lit;
 
-    public CopperTorchBlock(Settings settings, DefaultParticleType particle) {
-        super(settings, particle);
-        this.setDefaultState(this.stateManager.getDefaultState().with(LIT, true));
+    public CopperTorchBlock(Settings settings, boolean lit) {
+        super(settings, ParticleTypes.SOUL_FIRE_FLAME);
+        this.lit = lit;
     }
 
     public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
-        super.onBlockAdded(state, world, pos, oldState, notify);
-        world.createAndScheduleBlockTick(pos, world.getBlockState(pos).getBlock(), tickDelay);
+        if (lit) {
+            world.createAndScheduleBlockTick(pos, world.getBlockState(pos).getBlock(), tickDelay);
+        }
     }
 
     public static void transmitPowerUp(ServerWorld world, BlockPos pos) {
@@ -52,16 +48,16 @@ public class CopperTorchBlock extends TorchBlock {
     }
 
     public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        super.scheduledTick(state, world, pos, random);
-        world.createAndScheduleBlockTick(pos, world.getBlockState(pos).getBlock(), tickDelay);
-        transmitPowerUp(world, pos);
+        if (lit) {
+            world.createAndScheduleBlockTick(pos, world.getBlockState(pos).getBlock(), tickDelay);
+            transmitPowerUp(world, pos);
+        }
     }
 
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{LIT});
-    }
-
-    static {
-        LIT = Properties.LIT;
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        if (lit) {
+            super.randomDisplayTick(state, world, pos, random);
+        }
     }
 }
--- src/main/java/com/tassaron/copperlamp/block/WallCopperTorchBlock.java
+++ src/main/java/com/tassaron/copperlamp/block/WallCopperTorchBlock.java
@@ -4,46 +4,45 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.WallTorchBlock;
-import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Property;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 import java.util.Random;
 
 public class WallCopperTorchBlock extends WallTorchBlock {
     public static final DirectionProperty FACING;
-    public static final BooleanProperty LIT;
+    public boolean lit;
 
-    public WallCopperTorchBlock(Settings settings, DefaultParticleType particle) {
-        super(settings, particle);
-        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH).with(LIT, true));
-        this.lootTableId = new Identifier("copperlamp","blocks/copper_torch");
+    public WallCopperTorchBlock(Settings settings, boolean lit) {
+        super(settings, ParticleTypes.SOUL_FIRE_FLAME);
+        this.lit = lit;
+        this.lootTableId = new Identifier("copperlamp", lit ? "blocks/lit_copper_torch" : "blocks/copper_torch");
     }
 
     public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
-        super.onBlockAdded(state, world, pos, oldState, notify);
-        world.createAndScheduleBlockTick(pos, world.getBlockState(pos).getBlock(), CopperTorchBlock.tickDelay);
+        if (lit) {
+            world.createAndScheduleBlockTick(pos, world.getBlockState(pos).getBlock(), CopperTorchBlock.tickDelay);
+        }
     }
 
     public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        super.scheduledTick(state, world, pos, random);
-        world.createAndScheduleBlockTick(pos, world.getBlockState(pos).getBlock(), CopperTorchBlock.tickDelay);
-        CopperTorchBlock.transmitPowerUp(world, pos);
+        if (lit) {
+            world.createAndScheduleBlockTick(pos, world.getBlockState(pos).getBlock(), CopperTorchBlock.tickDelay);
+            CopperTorchBlock.transmitPowerUp(world, pos);
+        }
     }
 
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{FACING, LIT});
+        builder.add(new Property[]{FACING});
     }
 
     static {
         FACING = HorizontalFacingBlock.FACING;
-        LIT = CopperTorchBlock.LIT;
     }
 }
